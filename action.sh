aa4673f5fe03265c9acf5bb15c4f62decde81d69
RekGRpth <rekgrpth@gmail.com>
2021-08-05 19:18:33 +0500

    op

--- action.sh
+++ action.sh
@@ -2,8 +2,8 @@
 
 set -eux
 env | sort
-git remote add --fetch --track "${INPUTS_BRANCH:-${GITHUB_REF##*/}}" upstream "${INPUTS_URL}${INPUTS_REPOSITORY}.git"
-git config --local user.email "${INPUTS_EMAIL}"
-git config --local user.name "${INPUTS_NAME}"
+git remote add --fetch --track "${INPUTS_BRANCH:-${GITHUB_REF##*/}}" upstream "${INPUTS_URL:-${GITHUB_SERVER_URL}}${INPUTS_REPOSITORY}.git"
+git config --local user.email "${INPUTS_EMAIL:-actions@github.com}"
+git config --local user.name "${INPUTS_NAME:-git merge upstream}"
 git merge --allow-unrelated-histories "upstream/${INPUTS_BRANCH:-${GITHUB_REF##*/}}"
 git push --progress --set-upstream origin "${INPUTS_BRANCH:-${GITHUB_REF##*/}}"
