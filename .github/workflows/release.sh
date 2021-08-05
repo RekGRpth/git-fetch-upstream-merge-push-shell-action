aa1035456a108285c4e9e1d3491ed7f8d5e6f040
RekGRpth <rekgrpth@gmail.com>
2021-08-05 20:29:43 +0500

    up

--- .github/workflows/release.sh
+++ .github/workflows/release.sh
@@ -2,5 +2,5 @@
 
 set -eux
 hub release delete "${GITHUB_REF##*/}" || echo "$?"
-hub api --method DELETE "repos/${GITHUB_REPOSITORY}/git/refs/${GITHUB_REF##*/}" | jq .
+hub api --method DELETE "repos/${GITHUB_REPOSITORY}/git/refs/tags/${GITHUB_REF##*/}" | jq .
 hub release create --message "${INPUTS_MESSAGE:-release}" "${GITHUB_REF##*/}"
