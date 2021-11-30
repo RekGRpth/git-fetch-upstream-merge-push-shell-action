4081e43ba7ca4ef17c905cb1e135ba20dd6ada61
RekGRpth <rekgrpth@gmail.com>
2021-11-30 15:21:15 +0500

    keepalive

--- action.sh
+++ action.sh
@@ -1,8 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 set -eux
 git remote add --fetch --track "${INPUTS_BRANCH:-${GITHUB_REF##*/}}" upstream "${INPUTS_URL:-${GITHUB_SERVER_URL}}/${INPUTS_REPOSITORY}.git"
 git config --local user.email "${INPUTS_EMAIL:-actions@github.com}"
 git config --local user.name "${INPUTS_NAME:-git merge upstream}"
 git merge --allow-unrelated-histories "upstream/${INPUTS_BRANCH:-${GITHUB_REF##*/}}"
+let DIFF=($(date +%s)-$(git --no-pager log -1 --format=%ct))/86400
+if [ $DIFF -ge 50 ]; then
+    git commit --allow-empty --message keepalive
+fi
 git push --progress
